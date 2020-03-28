3a6b8dc0bd2873da89bc285375e8c0cc3fba0049
joaovictor900 <victorsantos900@gmail.com>
2020-03-28 19:16:12 -0300

    Implemented: page with all posts and full view of the post. Page with a post registration form.

--- src/main/java/br/com/codeblog/codeblog/controllers/CodeblogController.java
+++ src/main/java/br/com/codeblog/codeblog/controllers/CodeblogController.java
@@ -3,13 +3,19 @@
  */
 package br.com.codeblog.codeblog.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.codeblog.codeblog.model.Post;
 import br.com.codeblog.codeblog.service.CodeblogService;
@@ -27,9 +33,33 @@ public class CodeblogController {
 	
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
 	public ModelAndView getPost() {
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts  = codeblogService.findAll();
 		mv.addObject("posts", posts);
 		return mv;
 	}
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post  = codeblogService.findByID(id);
+		mv.addObject("post", post);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm () {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		codeblogService.save(post);
+		return "redirect:/posts";
+	}	
 }
